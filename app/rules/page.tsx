0b3dc8760fab59ac9768d1102a49844db33f474c
DevFoxie <hmerniz.dev@gmail.com>
2024-03-18 12:25:47 +0100

    fixing deployment bugs

--- app/rules/page.tsx
+++ app/rules/page.tsx
@@ -14,7 +14,7 @@ export default function RulesPage() {
 				</div>
 				<p style={{ fontSize: "1.2rem", marginTop: 50 }}><span className="text-pink-500">Respect:</span> Treat all FC members with kindness, respect, and consideration. Discrimination, harassment, or any form of disrespectful behavior will not be tolerated.</p>
 				<p style={{ fontSize: "1.2rem", marginTop: 20 }}><span className="text-pink-500">Toxicity:</span> We do not tolerate any form of toxic behavior. This includes but is not limited to: trolling, flaming, or any form of harassment.</p>
-				<p style={{ fontSize: "1.2rem", marginTop: 20 }}><span className="text-pink-500">Helping:</span> We encourage all members to help each other out. If you need help with something, don't hesitate to ask in the FC chat.</p>
+				<p style={{ fontSize: "1.2rem", marginTop: 20 }}><span className="text-pink-500">Helping:</span> We encourage all members to help each other out. If you need help with something, don&apos;t hesitate to ask in the FC chat.</p>
 				<p style={{ fontSize: "1.2rem", marginTop: 20 }}><span className="text-pink-500">Chat:</span> Keep the FC chat friendly and respectful. No spamming, advertising, or any form of inappropriate content.</p>
 				<p style={{ fontSize: "1.2rem", marginTop: 20 }}><span className="text-pink-500">Discord:</span> We have a Discord server for the FC. We encourage all members to join and participate in the server.</p>
 				<p style={{ fontSize: "1.2rem", marginTop: 20 }}><span className="text-pink-500">Having fun</span> And most importantly we encourage all members to enjoy their time in the FC and to be happy!</p>
